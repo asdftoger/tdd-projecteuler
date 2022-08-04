025f80897f377b655b887a7ba7c180027646d862
asdftoger <asdftoger@gmail.com>
2022-08-04 16:12:37 +1000

    change prime to search until sqrt

--- project_euler.py
+++ project_euler.py
@@ -1,3 +1,45 @@
-
+# %%
+import math
+#%%
 def sum_natural_multiples(n: int, mults: list[int]):
-    return 0
+    acc = 0
+    for i in range(n+1):
+        for mult in mults:
+            if i % mult == 0:
+                acc += i
+                break
+    return acc
+
+
+def fib_even_sum(n_max):
+    a, b = 1, 2
+    n_sum = 0
+    while b < n_max:
+        a, b = b, a+b
+        if a % 2 == 0:
+            n_sum += a
+    return n_sum
+
+
+def largest_prime_factor(n):
+    prime_factors=[]
+    for i in range(2,math.ceil(math.sqrt(n))):
+        if n % i ==0: 
+            for j in prime_factors:
+                if i%j==0:
+                    break
+            else:
+                prime_factors.append(i)
+    
+    #Empty list
+    return prime_factors[-1] if prime_factors else n
+
+def is_prime(n):
+    for i in range(2,int(n/2)+1):
+        if n % i ==0: 
+            return False
+
+    return True
+
+
+# %%
--- tests/unit/test_project_euler.py
+++ tests/unit/test_project_euler.py
@@ -1,9 +1,26 @@
 import pytest
-
-from project_euler import sum_natural_multiples
+from project_euler import fib_even_sum, is_prime, sum_natural_multiples, largest_prime_factor
 
 
 def test_problem1():
     assert sum_natural_multiples(n=5, mults=[3, 5]) == 8
     assert sum_natural_multiples(n=9, mults=[3, 5]) == 23
     assert sum_natural_multiples(n=10, mults=[3, 5]) == 33
+
+
+def test_problem2():
+
+    assert fib_even_sum(10) == 10
+    assert fib_even_sum(33) == 10
+    assert fib_even_sum(88) == 44
+
+
+def test_problem3():
+    assert largest_prime_factor(13195) == 29
+    assert largest_prime_factor(5) == 5
+    assert largest_prime_factor(12) == 3
+
+
+def test_is_prime():
+    assert is_prime(11)
+    assert not is_prime(10)
