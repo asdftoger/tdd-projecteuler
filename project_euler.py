5b34bffaaef1ed995630ebff7a20a4852d6a2071
asdftoger <asdftoger@gmail.com>
2022-08-04 14:47:42 +1000

    failed test

---
+++ project_euler.py
@@ -0,0 +1,3 @@
+
+def sum_natural_multiples(n: int, mults: list[int]):
+    return 0
--- tests/unit/test_project_euler.py
+++ tests/unit/test_project_euler.py
@@ -1,1 +1,9 @@
 import pytest
+
+from project_euler import sum_natural_multiples
+
+
+def test_problem1():
+    assert sum_natural_multiples(n=5, mults=[3, 5]) == 8
+    assert sum_natural_multiples(n=9, mults=[3, 5]) == 23
+    assert sum_natural_multiples(n=10, mults=[3, 5]) == 33
